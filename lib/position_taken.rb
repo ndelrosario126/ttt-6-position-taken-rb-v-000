c7f71d8fed6b00c65616e573d4282efcccaebb87
n.delrosario126@gmail.com <n.delrosario126@gmail.com>
2018-08-10 21:43:23 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(board, index = 0)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
+    elsif board[index] == "X" || board[index] == "O"
+    true
 end
 end
